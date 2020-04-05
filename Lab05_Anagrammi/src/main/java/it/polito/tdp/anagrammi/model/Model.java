38250c3349958a19f41776ef69bbd5965224bc0b
Marco <Marco@LAPTOP-A0R736MS.homenet.telecomitalia.it>
2020-04-05 11:55:58 +0200

    solo tolto qualche warning

--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/model/Model.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/model/Model.java
@@ -1,7 +1,6 @@
 package it.polito.tdp.anagrammi.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
--- Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/model/Parola.java
+++ Lab05_Anagrammi/src/main/java/it/polito/tdp/anagrammi/model/Parola.java
@@ -1,9 +1,7 @@
 package it.polito.tdp.anagrammi.model;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Parola {
 	
